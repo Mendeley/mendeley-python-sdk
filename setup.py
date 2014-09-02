2300bd970de91c13b899f50b5f15c0d2cefaecb4
Matt Thomson <matt.thomson@mendeley.com>
2014-09-02 10:53:52 +0100

    Return to using latest versions, now vcrpy is fixed.

--- setup.py
+++ setup.py
@@ -15,15 +15,15 @@ setup(
     description='Python SDK for the Mendeley API',
 
     install_requires=[
-        'arrow==0.4.4',
-        'future==0.13.0',
-        'memoized-property==1.0.2',
-        'requests==2.3.0',
-        'requests-oauthlib==0.4.1',
+        'arrow',
+        'future',
+        'memoized-property',
+        'requests',
+        'requests-oauthlib',
     ],
 
     tests_require=[
-        'pytest==2.6.1',
-        'vcrpy==1.0.2'
+        'pytest',
+        'vcrpy'
     ]
 )
